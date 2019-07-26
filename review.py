adb9a5c97165b554145a82b2a0c88f109aefe2d2
Hua <baobao357@gmail.com>
2019-07-26 22:11:08 +0800

    print 進度

--- review.py
+++ review.py
@@ -3,8 +3,7 @@ data = []
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
+		print(len(data))  #印出列印的進度
 
-print(data) #印出全部的資料
-
-
-
+print(data[0])   #印出第0筆是什麼
+print(len(data)) #印出data清單的總數
