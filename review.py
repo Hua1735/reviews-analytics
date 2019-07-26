b91fb9eeb5106d26a87918cc2442b81df23c3f4f
Hua <baobao357@gmail.com>
2019-07-26 22:16:33 +0800

    每一千印筆印一次

--- review.py
+++ review.py
@@ -1,9 +1,12 @@
 data = []
+count = 0
 
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
-		print(len(data))  #印出列印的進度
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))  #印出列印的進度
 
 print(data[0])   #印出第0筆是什麼
 print(len(data)) #印出data清單的總數
